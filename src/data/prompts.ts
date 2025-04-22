630cb2e3866b4861f0e7aa3a7988a8728920d503
axxessgranted <gkantor123@gmail.com>
2025-04-22 19:39:46 +0900

    add some basic prompts

---
+++ src/data/prompts.ts
@@ -0,0 +1,11 @@
+export const prompts = [
+  "What’s your name?",
+  "What do you like?",
+  "Say 3 fruits.",
+  "Ask your friend a question.",
+  "What day is it today?",
+  "What animal do you like?",
+  "Say the alphabet from A to E.",
+  "What’s your favorite color?",
+  "Say 3 things you see.",
+];
